112c2c6bf386df0de51307b6f89b231ad5e3299f
Jinhan Park <jinhan92park@gmail.com>
2017-10-28 17:06:27 +0900

    10_flags cpp got better

--- 10_prime_and_composite_numbers/flags.cpp
+++ 10_prime_and_composite_numbers/flags.cpp
@@ -1,43 +1,46 @@
-#include "vector"
-#include "algorithm"
+#include <vector>
 
 using namespace std;
 
-int solution(vector<int> &a)
-{
+vector<int> peak_gaps(vector<int> &a) {
   int n = a.size();
-  int start = -1;
-  int end = -1;
-  vector<bool> peaks(n, 0);
-  for (int i = 1; i < n-1; ++i){
-    if (a[i] > a[i-1] && a[i] > a[i+1]){
-      peaks[i] = 1;
-      if (start == -1)
-        start = i;
-      end = i;
+  int cnt = 0;
+  vector<int> ret;
+  for (int i = 1; i < n - 1; i++) {
+    cnt++;
+    if (a[i] > a[i - 1] && a[i] > a[i + 1]) {
+      ret.push_back(cnt);
+      cnt = 0;
     }
   }
+  return ret;
+}
 
-  if (start == -1)
-    return 0;
-  else if (start == end)
-    return 1;
-
-  int max_step = 0;
-  while (max_step * (max_step + 1) < n)
-    ++max_step;
-  
-
-  for (int step = max_step; step > 2; --step){
-    int cnt = step - 1;
-    for (int i = start + step; i <= end; ++i){
-      if (peaks[i]){
-        --cnt;
-        if (cnt == 0)
-          return step;
-        i += step - 1;
+int solution(vector<int> &a) {
+  int ret = 0;
+  int n = a.size();
+  vector<int> pgaps = peak_gaps(a);
+  int m = pgaps.size();
+  int remained, step;
+  while (ret < m && ret * (ret + 1) < n) {
+    ret++;
+  }
+  if (ret < 3) {
+    return ret;
+  }
+  for (; ret > 2; ret--) {
+    remained = ret - 1;
+    step = 0;
+    for (int i = 1; i < m; i++) {
+      step += pgaps[i];
+      if (step >= ret) {
+        step = 0;
+        remained--;
+        if (remained == 0) {
+          return ret;
+        }
       }
     }
   }
-  return 2;
+  return ret;
 }
