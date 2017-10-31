4272a4836950e854397a8e75214fa55322bc84dd
Jinhan Park <jinhan92park@gmail.com>
2017-10-31 09:45:39 +0900

    12_chocolates by numbers cpp improved

--- 12_euclidean_algorithm/chocolates_by_numbers.cpp
+++ 12_euclidean_algorithm/chocolates_by_numbers.cpp
@@ -1,12 +1,20 @@
-int solution(int n, int m)
-{
-  int a = max(n, m);
+#include <algorithm>
+
+using namespace std;
+
+int gcd(int n, int m) {
+  int a= max(n, m);
   int b = min(n, m);
-  int c = 1;
-  while (b != 0){
+  int c;
+  while (b != 0) {
     c = a % b;
     a = b;
     b = c;
   }
-  return n / a;
+  return a;
+}
+
+int solution(int n, int m) {
+  int g = gcd(n, m);
+  return n / g;
 }
