be748eef0c731ceb7fd83fdbf821eb1ceceb04e7
Jinhan Park <jinhan92park@gmail.com>
2017-10-31 09:58:07 +0900

    12_common prime divisors cpp small changes

--- 12_euclidean_algorithm/common_prime_divisors.cpp
+++ 12_euclidean_algorithm/common_prime_divisors.cpp
@@ -3,48 +3,46 @@
 
 using namespace std;
 
-int calc_gcd(int a, int b)
-{
-  int c = max(a, b);
-  int d = min(a, b);
-  int e;
-  while (d != 0){
-    e = c % d;
-    c = d;
-    d = e;
+int gcd(int n, int m) {
+  int a= max(n, m);
+  int b = min(n, m);
+  int c;
+  while (b != 0) {
+    c = a % b;
+    a = b;
+    b = c;
   }
-  return c;
+  return a;
 }
 
-int remove_factor(int a, int b)
-{
-  while (a % b == 0){
+int remove_factor(int a, int b) {
+  while (a % b == 0) {
     a /= b;
   }
   return a;
 }
 
-int solution(vector<int> &a, vector<int> &b)
-{
+
+int solution(vector<int> &a, vector<int> &b) {
   int ret = 0;
   int z = a.size();
-  for (int i = 0; i < z; ++i){
+  for (int i = 0; i < z; i++) {
     int l = a[i];
     int r = b[i];
     if (l == r){
-      ++ret;
+      ret++;
       continue;
     }
-    int gcd = calc_gcd(l, r);
-    for (int j = 2; gcd != 1 || j * j <= gcd; ++j){
-      if (gcd % j == 0){
-        gcd = remove_factor(gcd, j);
+    int g = gcd(l, r);
+    for (int j = 2; g != 1 || j * j <= g; j++) {
+      if (g % j == 0){
+        g = remove_factor(g, j);
         l = remove_factor(l, j);
         r = remove_factor(r, j);
       }
     }
     if (l == 1 && r == 1)
-      ++ret;
+      ret++;
   }
   return ret;
 }
