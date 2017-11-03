1e18d6f64fa7588e750f338ed792deea3623c60a
Jinhan Park <jinhan92park@gmail.com>
2017-11-03 18:24:54 +0900

    14_nailing_planks_py improved

--- 14_binary_search_algorithm/nailing_planks.py
+++ 14_binary_search_algorithm/nailing_planks.py
@@ -1,39 +1,26 @@
-def check(A, B, cnts):
-  N = len(A)
-  i = 0
-  for i in xrange(N):
-    if cnts[B[i]] - cnts[A[i] - 1] == 0:
+def check(v, a, b, c):
+  n = len(a)
+  m = len(c)
+  nail_cnts = [0] * (2 * m + 1)
+  for i in range(v):
+    nail_cnts[c[i]] = 1
+  for i in range(1, 2 * m + 1):
+    nail_cnts[i] = nail_cnts[i - 1] + nail_cnts[i]
+  for i in range(n):
+    if nail_cnts[b[i]] - nail_cnts[a[i] - 1] == 0:
       return 0
   return 1
 
-def solution(A, B, C):
-  M = len(C)
-  N = len(A)
-
+def solution(a, b, c):
+  m = len(c)
   beg = 1
-  end = M
-
-  ret = -1
-
+  end = m
+  rt = -1
   while beg <= end:
     mid = (beg + end) / 2
-    nails = [0] * (2 * M + 1)
-    for i in C[:mid]:
-      nails[i] = 1
-
-    cnt = 0
-    cnts = [0] * (2 * M + 1)
-    i = 0
-    while i < 2 * M + 1:
-      if nails[i] == 1:
-        cnt += 1
-      cnts[i] = cnt
-      i += 1
-      
-    if check(A, B, cnts):
-      ret = mid
+    if check(mid, a, b, c):
+      rt = mid
       end = mid - 1
     else:
       beg = mid + 1
-
-  return ret
+  return rt
