624d61c287fcb8f35fb177e2fad1c271c1c89caf
Jinhan Park <jinhan92park@gmail.com>
2017-11-03 18:24:39 +0900

    14_nailing_planks_cpp 100

---
+++ 14_binary_search_algorithm/nailing_planks.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+
+using namespace std;
+
+bool check(int v, vector<int> &a, vector<int> &b, vector<int> &c) {
+  int n = a.size();
+  int m = c.size();
+  vector<int> nail_cnts(2 * m + 1, 0);
+  for (int i = 0; i < v; i++) {
+    nail_cnts[c[i]] = 1;
+  }
+  for (int i = 1; i < 2 * m + 1; i++) {
+    nail_cnts[i] = nail_cnts[i - 1] + nail_cnts[i];
+  }
+  for (int i = 0; i < n; i++) {
+    if (nail_cnts[b[i]] - nail_cnts[a[i] - 1] == 0) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+int solution(vector<int> &a, vector<int> &b, vector<int> &c) {
+  int m = c.size();
+  
+  int beg = 1;
+  int end = m;
+  int mid;
+  int rt = -1;
+  while (beg <= end) {
+    mid = (beg + end) / 2;
+    if (check(mid, a, b, c)) {
+      rt = mid;
+      end = mid - 1;
+    }
+    else {
+      beg = mid + 1;
+    }
+  }
+  return rt;
+}
