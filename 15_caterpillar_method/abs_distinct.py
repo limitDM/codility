886298c7ff81cc11669dd5b637e1d6ba00f2bc9e
Jinhan Park <jinhan92park@gmail.com>
2017-11-04 11:43:16 +0900

    15_abs_distict py improved

--- 15_caterpillar_method/abs_distinct.py
+++ 15_caterpillar_method/abs_distinct.py
@@ -1,45 +1,39 @@
-def solution(a):
-  n = len(a)
+def remove_duplicate(a):
+  rt = []
+  last = a[0] + 1;
+  for i in a:
+    if i != last:
+      last = i
+      rt.append(i)
+  return rt
 
-  if a[0] >=0 or a[n-1] <= 0:
-    last_seen = a[0]
-    cnt = 1
-    for i in xrange(1, n):
-      if a[i] != last_seen:
-        cnt += 1
-        last_seen = a[i]
-    return cnt
-    
-  beg = 0
+def search_smallest_nonnegative(a):
+  n = len(a)
   end = n-1
-    
-  
-  while beg <= end:
+  beg = 0
+  while (beg <= end):
     mid = (beg + end) / 2
-    if a[mid] >= 0:
-      center = mid
-      end = mid - 1
+    if (a[mid] >= 0):
+      rt = mid
+      end = mid -1
     else:
       beg = mid + 1
-
-  beg = 0
-  end = n-1
-  last_seen = max(-a[beg], a[end])
-  cnt = 1
-
-  while end >= center:
-    while -a[beg] >= a[end]:
-      if -a[beg] != last_seen:
-        last_seen = -a[beg]
-        cnt += 1
-      beg += 1
-    if a[end] != last_seen:
-      last_seen = a[end]
-      cnt += 1
-    end -= 1
-  while beg < center:
-    if -a[beg] != last_seen:
-      last_seen = -a[beg]
-      cnt += 1
-    beg += 1
-  return cnt
+  return rt
+  
+def solution(a):
+  a = remove_duplicate(a)
+  n = len(a)
+  rt = n
+  if a[0] >=0 or a[n-1] <= 0:
+    return rt
+  end = search_smallest_nonnegative(a);
+  beg = end - 1
+  while beg >= 0 and end < n:
+    if - a[beg] < a[end]:
+      beg -= 1
+    elif - a[beg] > a[end]:
+      end += 1
+    else:
+      end += 1
+      rt -= 1
+  return rt
