fb40aa7bbd2b6ded17427d82efd19cdf72d1dcc8
Jinhan Park <jinhan92park@gmail.com>
2017-11-04 12:35:57 +0900

    15_abs_distinct improved

--- 15_caterpillar_method/abs_distinct.cpp
+++ 15_caterpillar_method/abs_distinct.cpp
@@ -14,44 +14,26 @@ vector<int> remove_duplicate(vector<int> &a) {
   return rt;
 }
 
-int search_smallest_nonnegative(vector<int> &a) {
-  int n = a.size();
-  int end = n - 1;
-  int beg = 0;
-  int mid;
-  int rt = 0;
-  while (beg <= end) {
-    mid = (beg + end) / 2;
-    if (a[mid] >= 0) {
-      rt = mid;
-      end = mid - 1;
-    } else {
-      beg = mid + 1;
-    }
-  }
-  return rt;
-}
-
 int solution(vector<int> &a) {
   a = remove_duplicate(a);
   int n = a.size();
   int rt = n;
   int beg, end;
-  if (a[n - 1] <= 0 || a[0] >= 0) {
+  if (a[0] >= 0 || a[n - 1] <= 0) {
     return rt;
   }
-  end = search_smallest_nonnegative(a);
-  beg = end - 1;
-  while (beg >= 0 and end < n) {
-    if (- a[beg] < a[end]) {
-      beg--;
-    }
-    else if (- a[beg] > a[end]) {
-      end++;
-    }
-    else {
+  end = n - 1;
+  beg = 0;
+  while (a[end] >= 0 and a[beg] < 0) {
+    if (a[beg] > - a[end]) {
+      // because of overflow issue, compare between negative numbers
+      // |MIN_INT| > MAX_INT
+      end--;
+    } else if (a[beg] < - a[end]) {
+      beg++;
+    } else {
+      end--;
       rt--;
-      end++;
     }
   }
   return rt;
--- 15_caterpillar_method/abs_distinct.py
+++ 15_caterpillar_method/abs_distinct.py
@@ -7,33 +7,20 @@ def remove_duplicate(a):
       rt.append(i)
   return rt
 
-def search_smallest_nonnegative(a):
-  n = len(a)
-  end = n-1
-  beg = 0
-  while (beg <= end):
-    mid = (beg + end) / 2
-    if (a[mid] >= 0):
-      rt = mid
-      end = mid -1
-    else:
-      beg = mid + 1
-  return rt
-  
 def solution(a):
   a = remove_duplicate(a)
   n = len(a)
   rt = n
-  if a[0] >=0 or a[n-1] <= 0:
+  if a[0] >= 0 or a[n - 1] <= 0:
     return rt
-  end = search_smallest_nonnegative(a);
-  beg = end - 1
-  while beg >= 0 and end < n:
+  end = n - 1
+  beg = 0
+  while a[end] >= 0 and a[beg] < 0:
     if - a[beg] < a[end]:
-      beg -= 1
+      end -= 1
     elif - a[beg] > a[end]:
-      end += 1
+      beg += 1
     else:
-      end += 1
+      end -= 1
       rt -= 1
   return rt
