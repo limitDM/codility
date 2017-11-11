f4f7006439875bbd10c9045ad890b5c5ac6473a5
Jinhan Park <jinhan92park@gmail.com>
2017-11-12 01:02:59 +0900

    15_min_abs_sum_of_two improved and cpp version added

--- 15_caterpillar_method/min_abs_sum_of_two.py
+++ 15_caterpillar_method/min_abs_sum_of_two.py
@@ -1,33 +1,15 @@
 def solution(a):
   n = len(a)
-  a.sort()
-
+  rt = int(2e9)
   beg = 0
-  end = n-1
-
-  center = None
-  while beg <= end:
-    mid = (beg + end) / 2
-
-    if a[mid] <= 0:
-      center = mid
-      beg = mid + 1
+  end = n - 1
+  a.sort()
+  while (beg < end):
+    rt = min(rt, abs(a[beg] + a[end]))
+    if (abs(a[beg]) < abs(a[end])):
+      end -= 1
+    elif (abs(a[beg]) > abs(a[end])):
+      beg += 1
     else:
-      end = mid - 1
-
-  if center == None:
-    return 2 * a[0]
-  elif center == n-1:
-    return -2 * a[-1]
-  else:
-    left = center
-    right = center + 1
-    ret = a[right] - a[left]
-    while right < n:
-      while left >= 0 and -a[left] < a[right]:
-        left -= 1
-      ret = min(ret,
-                min(abs(a[right] + a[left]),
-                    abs(a[right] + a[left + 1])))
-      right += 1
-  return ret
+      return 0
+  return min(rt, abs(a[beg] + a[end]))
